e6dad10a053c6c80e7a6d5434e8c9cecea947aa5
bretondev <33033418+bretondev@users.noreply.github.com>
2023-05-08 12:12:31 +0200

    Commit 6

--- 01 - Notions de base/finish/script.js
+++ 01 - Notions de base/finish/script.js
@@ -3,6 +3,7 @@
 Lier un fichier JavaScript
 ********************************
 */
+//Ces messages vont s'imprimer dans la console
 let welcomeMessage = "Bienvenue dans ce cours";
 console.log(welcomeMessage);
 console.log(4 + 5);
